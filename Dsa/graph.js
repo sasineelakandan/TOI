e4d8428ea7b7ccea9ea9e124e24d4aae04642fff
sasineelakandan <devasasi887@gmail.com>
2025-04-13 19:39:24 +0530

    bfs and dfs finish in graph

--- Dsa/graph.js
+++ Dsa/graph.js
@@ -1,34 +1,70 @@
-
-
-class Graph{
-    constructor(){
-        this.adjacencyList={}
+class Graph {
+    constructor() {
+      this.adjacencyList = {};
     }
-
-    addVertex(v){
-        if(!this.adjacencyList[v]){
-            this.adjacencyList[v]=[]
-        }
+  
+    addVertex(vertex) {
+      if (!this.adjacencyList[vertex]) {
+        this.adjacencyList[vertex] = [];
+      }
     }
-
-    addEddges(v1,v2){
-      if(!this.adjacencyList[v1])this.addVertex(v1)
-      if(!this.adjacencyList[v2])this.addVertex(v2)
-      this.adjacencyList[v1].push(v2)
-      this.adjacencyList[v2].push(v1)
+  
+    addEdges(v1, v2) {
+      if (!this.adjacencyList[v1]) this.addVertex(v1);
+      if (!this.adjacencyList[v2]) this.addVertex(v2); // fixed here
+  
+      this.adjacencyList[v1].push(v2); // fixed here
+      this.adjacencyList[v2].push(v1); // fixed here
     }
-}
-
-
-
-const graph=new Graph()
-
-graph.addVertex('A')
-
-graph.addVertex('B')
-graph.addVertex('C')
-
-graph.addEddges('A','B')
-graph.addEddges('B','C')
-graph.addEddges('A','C')
-console.log(graph.adjacencyList)
+  
+    bfs(start){
+      
+      let queue=[start]
+      let visited={}
+      let data=[]
+      visited[start]=true
+      while(queue.length){
+        let curr=queue.shift()
+        data.push(curr)
+        this.adjacencyList[curr].forEach((val)=> {
+          if(!visited[val]){
+            visited[val]=true
+            queue.push(val)
+          }
+        });
+      }
+      return data
+    }
+  
+    dfs(start){
+      
+      let stack=[start]
+      let visited={}
+      let data=[]
+      visited[start]=true
+      while(stack.length){
+        let curr=stack.pop()
+        data.push(curr)
+        this.adjacencyList[curr].forEach((val)=> {
+          if(!visited[val]){
+            visited[val]=true
+            stack.push(val)
+          }
+        });
+      }
+      return data
+    }
+  }
+  
+  const graph = new Graph();
+  
+  graph.addVertex('A');
+  graph.addVertex('B');
+  graph.addVertex('C');
+  
+  graph.addEdges('A', 'B');
+  graph.addEdges('B', 'C');
+  graph.addEdges('A', 'C');
+  console.log(graph.dfs('B'))
+  console.log(graph.adjacencyList);
+  
